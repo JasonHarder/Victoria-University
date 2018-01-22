7f8c7f0fb367d3d57a5487b004d730decf0f8664
Anthematics <jasonharder2@gmail.com>
2018-01-21 21:07:33 -0500

    removed turbolinks and merged branch with master which cleared up issue of real time chat not working

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,12 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users
-
-  get "register" => "devise/registrations#new"
-  get "edit_profile" => "devise/registrations#edit"
-  get "login" => "devise/sessions#new"
-  get "logout" => "devise/sessions#destroy"
-
+  
   resources :conversations, only: [:create] do
     member do
       post :close
     end
     resources :messages, only: [:create]
+    
   end
-
-
-
 end
