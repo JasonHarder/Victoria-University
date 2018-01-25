fbf17434b2859105e67b34400082495c16380955
Anthematics <jasonharder2@gmail.com>
2018-01-25 15:13:09 -0500

    merged with Karthik's code (new toolbar)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,13 @@
-class RegistrationsController < ApplicationController
-   before_action :limit_users_registered
-  
-  def limit_users_registered
-    if (User.count == 5) & user_signed_in?
-      redirect_to root_path
-    else 
-      redirect_to new_user_session_path
+class RegistrationsController < Devise::RegistrationsController
+    before_action :limit_users, only: [:new, :create]
+
+    protected
+
+    def limit_users
+        if ((User.count == 5) & (user_signed_in?))
+            redirect_to root_path
+        elsif User.count ==5
+            redirect_to new_user_session_path
+        end
     end 
-  end
 end
--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get limit_users_registered" do
-    get registrations_limit_users_registered_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
