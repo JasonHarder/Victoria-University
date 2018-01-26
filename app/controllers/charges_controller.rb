8a74aaf262bc8ee13969c38bf820bb297812ffe6
Anthematics <jasonharder2@gmail.com>
2018-01-26 13:21:04 -0500

    reworked charges controller unsure of if its fixed

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,40 +1,39 @@
 class ChargesController < ApplicationController
       
-    def new
+  def new
+  end
+
+  def create
+    @amount = params[:amount]
+
+    @amount = @amount.gsub('$', '').gsub(',', '')
+
+    begin
+      @amount = Float(@amount).round(2)
+    rescue
+      flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
+      redirect_to new_charge_path
+      return
     end
 
-    def create
-      @amount = params[:amount]
+    @amount = (@amount * 100).to_i # Must be an integer!
 
-      @amount = @amount.gsub('$', '').gsub(',', '')
-
-      begin
-        @amount = Float(@amount).round(2)
-      rescue
-        flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
-        redirect_to new_charge_path
-        return
-      end
-
-      @amount = (@amount * 100).to_i # Must be an integer!
-
-      if @amount < 500
-        flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
-        redirect_to new_charge_path
-        return
-      end
-
-      Stripe::Charge.create(
-        :amount => @amount,
-        :currency => 'usd',
-        :source => params[:stripeToken],
-        :description => 'Custom donation'
-      )
-
-      rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to new_charge_path
-      end
+    if @amount < 500
+      flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
+      redirect_to new_charge_path
+      return
     end
-  
+
+    Stripe::Charge.create(
+      :amount => @amount,
+      :currency => 'usd',
+      :source => params[:stripeToken],
+      :description => 'Custom donation'
+    )
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    end
+  end
 end 
