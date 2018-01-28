b3458790bf5113ffa58a1e37ec5a42e868623998
Anthematics <jasonharder2@gmail.com>
2018-01-27 22:40:25 -0500

    Still improving the CRUD for events , but have to think about admin setups for all pages.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,39 +1,51 @@
 class EventsController < ApplicationController
+
   def index
     @events = Event.all
   end
 
   def show
-    @event = event.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
   def new
-  end 
-
-  def create
-    @event = Event.new(event_params)
-   
-      if @event.save
-        redirect_to @event
-      else
-        render 'new'
-      end
+    @event = Event.new
   end
 
   def edit
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
   end
 
   def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-  end
+    @event = Event.find(params[:id])
+    @event.destroy
 
+    redirect_to events_path
+  end
 
   private
 
-  def event_params
-    params.require(:event).permit(:title, :url, :body)
-  end 
-
+    def event_params
+      params.require(:event).permit(:title, :url, :body)
+    end
 end
