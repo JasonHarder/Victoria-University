59a2025caa57b74c5e892e90bcf381eb65217cd1
Anthematics <jasonharder2@gmail.com>
2018-01-30 18:57:05 -0500

    fixed a few rails errors (layout bugs)

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,28 @@
 Rails.application.routes.draw do
 
-
   root 'home#index'
-  get '/' => 'home#index' 
-  
+  get '/' => 'home#index'
+
+  get'interested'=> 'interested#index' 
+
   devise_for :users, controllers: {registrations: "registrations"}
 
   resources :events
 
   resources :charges
-    
+
   resources :home, only: [:index]
-  
+
   resources :campus, only: [:index]
-  
+
   resources :admission, only: [:index]
-  
+
   resources :schools, only: [:index]
-  
+
   resources :about, only: [:index]
 
   resources :contact, only: [:index]
-  
+
   resources :academic, only: [:index]
 
   resources :conversations, only: [:create] do
