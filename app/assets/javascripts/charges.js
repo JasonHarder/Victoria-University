de9a8951a739596c21bb54066983461e093828ea
Anthematics <jasonharder2@gmail.com>
2018-02-22 14:26:19 -0500

    working on custom payments feature

--- app/assets/javascripts/charges.js
+++ app/assets/javascripts/charges.js
@@ -1,26 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const handler = StripeCheckout.configure({
-    key: 'pk_test_6pRNASCoBoKtISHfeqd4xmuh'
-    image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-    locale: 'auto'
-    token: function(token){
-      //get the token id to your serverside code for use (ENV variable?)
-    }
-  })
-}
+<script src="https://checkout.stripe.com/checkout.js"></script>
 
-document.getElementById('Paymentfield').addEventListener('click',function(e){
-  handler.open({
-    name:'Stripe.com',
-    description: '2 widgets',
-    zipcode:true,
-    amount:2000
-  });
-  e.preventDefault();
-});
 
-window.addEventListener('popstate',function () {
-  handler.close();
+
+<script>
+var handler = StripeCheckout.configure({
+  key: '<%= Rails.configuration.stripe[:publishable_key] %>',
+  locale: 'auto',
+  name: 'Victoria University',
+  description: 'One-time donation',
+  token: function(token) {
+    $('input#stripeToken').val(token.id);
+    $('form').submit();
+  }
 });
+</script>
 
 // if we will be using BABEL that const can stay as is , but ensure babel transpiler is included otherwise switch back to es5.
