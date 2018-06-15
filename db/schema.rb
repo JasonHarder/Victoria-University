352fce2da17e20df5a227467a76ade5a10e927c7
Anthematics <Jasonharder2@gmail.com>
2018-06-15 09:10:42 -0400

    fixed image placement in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 20180422193437) do
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
-    t.integer "user_id"
-    t.integer "conversation_id"
+    t.bigint "user_id"
+    t.bigint "conversation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
@@ -90,14 +90,6 @@ ActiveRecord::Schema.define(version: 20180422193437) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["teacher_id"], name: "index_teacher_pictures_on_teacher_id"
-  end
-
-  create_table "teachers", force: :cascade do |t|
-    t.string "name"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
@@ -114,7 +106,7 @@ ActiveRecord::Schema.define(version: 20180422193437) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "reason"
-    t.integer "age"
+    t.integer "Age"
     t.string "phone_number"
     t.string "occupation"
     t.string "first_name"
@@ -124,4 +116,6 @@ ActiveRecord::Schema.define(version: 20180422193437) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
